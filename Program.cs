f96ce83ec50a441556eb9db1097d8b12ac328653
Michwds <michael.woods@live.ca>
2023-05-11 22:37:34 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Snake{
+    class Program{
+        static void Main(string[] args){
+            const int fieldLength = 50, fieldWidth = 50;
+            const char tile = '#';
+            string line = string.Concat(Enumerable.Repeat(tile, fieldLength));
+
+            while(true){
+                
+            }
+
+        }
+    }
+}
